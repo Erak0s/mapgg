e11092a994a93c1a7b0513fa52f82cc8daf27a57
Florian Goutieras <goutieras.florian@gmail.com>
2026-03-21 11:21:08 +0100

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://mapgg.netlify.app"
+  ]
+}));
 app.use(express.json());
 
 const STARTGG_API_KEY = "4084825f7edcee7e793552fbf5f46648";
